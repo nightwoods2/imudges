797c067c6031aa53cb5a21db4c91989c642fdbdb
DandelionFlowers <1551728654@qq.com>
2020-01-27 10:12:11 +0800

    网页第一个版本
    还没有全部开发完，希望可以提供意见

---
+++ HTML/Js/vidk.js
@@ -0,0 +1,16 @@
+/*返回顶部*/
+$(window).scroll(function(){
+    var sc=$(window).scrollTop();
+    var rwidth=$(window).width()
+    if(sc>0){
+        $("#goTopBtn").css("display","block");
+        $("#goTopBtn").css("left",(rwidth-36)+"px")
+    }else{
+        $("#goTopBtn").css("display","none");
+    }
+})
+$("#goTopBtn").click(
+    function(){
+    var sc=$(window).scrollTop();
+    $('body,html').animate({scrollTop:0},500);
+})
